cb9593d5f13544f50a6c215c42c01ecc9436dd27
Pro1ooEgor <e-dyb@mail.ru>
2019-06-02 22:14:06 +0300

    adding comment

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -8,18 +8,18 @@
       :author="data.author"
     ></article-view>
     <comment class="pt-5"></comment>
-<!--    <div class="row justify-content-center">-->
-<!--      <div class="col-8 pt-3 text-right">-->
-<!--        <button-->
-<!--          type="button"-->
-<!--          style="width:140px"-->
-<!--          class="btn btn-outline-success"-->
-<!--          @click="sendData"-->
-<!--        >-->
-<!--          Save-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
+    <div class="row justify-content-center">
+      <div class="col-8 pt-3 text-right">
+        <button
+          type="button"
+          style="width:140px"
+          class="btn btn-outline-success"
+          @click="sendData"
+        >
+          Save
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,6 +39,24 @@ export default {
   components: {
     articleView,
     comment
+  },
+  methods: {
+    sendData () {
+      axios
+        .post(baseUrl + 'articles/', {
+          title: this.title,
+          html: this.content || this.value,
+          text: this.text,
+          author: this.$store.getters.getAuthor.id
+        })
+        .then(response => {
+          console.log(response)
+          this.$refs.form.submit()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   },
   mounted () {
     console.log(baseUrl + 'article/' + this.$route.params.id + '/')
